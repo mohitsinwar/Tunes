13fcb185f555beca4715cc1f20bce904ba4d7dd7
mohitsinwar <mularam01012000@gmail.com>
2021-01-22 01:33:18 +0530

    background change

--- music/models.py
+++ music/models.py
@@ -1,16 +1,14 @@
+from django.contrib.auth.models import Permission, User
 from django.db import models
-from django.urls import reverse
 
 
-# Create your models here.
 class Album(models.Model):
-    artist = models.CharField(max_length=100)
-    album_title = models.CharField(max_length=400)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=900)
-
-    def get_absolute_url(self):
-        return reverse('music:detail', kwargs={'pk': self.pk})
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
@@ -18,8 +16,8 @@ class Album(models.Model):
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=10)
-    song_title = models.CharField(max_length=200)
+    song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
